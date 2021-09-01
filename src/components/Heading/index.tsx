a7f8d1984101c21cf3a9b659e674e305984875fa
lucasgmelo <contatolucasgmelo@gmail.com>
2021-09-01 14:02:32 -0300

    create sign in page

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -6,7 +6,7 @@ export type HeadingProps = {
   lineLeft?: boolean
   lineBottom?: boolean
   lineColor?: 'primary' | 'secondary'
-  size?: 'small' | 'medium'
+  size?: 'small' | 'medium' | 'huge'
 }
 
 const Heading = ({
